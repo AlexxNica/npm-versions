5e0f39196ba6bc4de89381f2eeed900da66d08e3
AlexxNica <kryndex@gmail.com>
2017-10-31 15:04:04 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as RegClient from 'npm-registry-client';
 import * as semver from 'semver';
 import * as chalk from 'chalk';
 
-const packageName = process.env.CUSTOM_PACKAGE_NAME;
+const packageName = (typeof process.argv[2] === 'string') && process.argv[2];
 const client = new RegClient({});
 const uri = "https://registry.npmjs.org/npm";
 const params = {
@@ -18,7 +18,7 @@ client.distTags.fetch(uri, params, function (error, data, raw, res) {
   const versionsListDummy: string[] = [];
 
   for (const prop in data) {
-    versionsListDummy.push(`${prop} = ${data[prop]}`);
+    versionsListDummy.push(`${data[prop]} - ${prop}`);
   };
 
   const versionsList = versionsListDummy.reduce(function (accumulator, currentValue) {
@@ -26,9 +26,11 @@ client.distTags.fetch(uri, params, function (error, data, raw, res) {
   });
 
   console.log();
-  console.log((<any>chalk).blue(`LATEST VERSIONS OF THE PACKAGE '${packageName}':`));
+  console.log((<any>chalk).grey('Argument passed through CLI: ' + process.argv[2]));
+  console.log((<any>chalk).bold.blueBright(`LATEST VERSIONS OF THE PACKAGE '${packageName}':`));
+      console.log((<any>chalk).greenBright(
+        (<any>Object).values(data).sort(semver.rcompare)[0])
+        + ' - Highest version regardless of channel: '
+      );
   console.log(versionsList);
-  console.log(
-    (<any>Object).values(data).sort(semver.rcompare)[0]
-  );
 });
