4c679add9f1bbe28e885e1d04f08c3010335fe2a
AlexxNica <kryndex@gmail.com>
2017-10-31 11:39:55 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 'use strict';
 
-import { spawn } from 'child_process';
-import * as npm from 'npm';
+import * as RegClient from 'npm-registry-client';
 
+var client = new RegClient({});
+var uri = "https://registry.npmjs.org/npm";
+var params = { timeout: 1000, }
 
-// console.log(npm.load());
-// console.log(npm.commands.view(['typescript', 'versions'], console.log));
-
-// var npmconf = require('../lib/config/core.js')
-// var configDefs = npmconf.defs
-// var shorthands = configDefs.shorthands
-// var conf = nopt(types, shorthands)
-
-
-
-console.log(JSON.stringify(npm.config.get('proxy')));
-// console.log(npm.commands.info);
+client.distTags.fetch(uri, params, function (error, data, raw, res) {
+  // error is an error if there was a problem.
+  // data is the parsed data object
+  // raw is the json string
+  // res is the response from couch
+  console.log('### DATA ###');
+  console.log(data);
+  console.log('### DATA ###');
+  console.log();
+  console.log('### RAW ###');
+  console.log(raw);
+  console.log('### RAW ###');
+});
