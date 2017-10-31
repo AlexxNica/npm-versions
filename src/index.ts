fbb2f67fc13179b2196f3a0a2e7679ea66337cd8
AlexxNica <kryndex@gmail.com>
2017-10-31 12:43:42 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 'use strict';
 
 import * as RegClient from 'npm-registry-client';
+import * as semver from 'semver';
 
-var client = new RegClient({});
-var uri = "https://registry.npmjs.org/npm";
-var params = { timeout: 1000, }
+const packageName = process.env.CUSTOM_PACKAGE_NAME;
+
+const client = new RegClient({});
+const uri = "https://registry.npmjs.org/npm";
+const params = {
+  timeout: 1000,
+  package: packageName,
+  auth: {},
+}
 
 client.distTags.fetch(uri, params, function (error, data, raw, res) {
-  // error is an error if there was a problem.
-  // data is the parsed data object
-  // raw is the json string
-  // res is the response from couch
-  console.log('### DATA ###');
-  console.log(data);
-  console.log('### DATA ###');
+  const dataArray = (<any>Object).entries(data);
+  const versionsList: string[] = [];
+
+
+  for (const prop in data) {
+    versionsList.push(`${prop} = ${data[prop]}`);
+  }
+
   console.log();
-  console.log('### RAW ###');
-  console.log(raw);
-  console.log('### RAW ###');
+  console.log(`Latest versions of the package ${packageName}:`);
+  console.log();
+  // console.log(semver.maxSatisfying(['5.4.1', '4.0.1', '1.0.0'], '*'));
+  console.log(
+    // (<any>Object).values(data).sort(semver.rcompare)[0]
+  );
 });
