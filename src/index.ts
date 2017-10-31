c217b0be79a5287c36c2aced5208b6db540299a0
AlexxNica <kryndex@gmail.com>
2017-10-31 14:11:28 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,6 @@ client.distTags.fetch(uri, params, function (error, data, raw, res) {
   console.log((<any>chalk).blue(`LATEST VERSIONS OF THE PACKAGE '${packageName}':`));
   console.log(versionsList);
   console.log(
-    // (<any>Object).values(data).sort(semver.rcompare)[0]
+    (<any>Object).values(data).sort(semver.rcompare)[0]
   );
 });
