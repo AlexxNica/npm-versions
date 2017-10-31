1fcba8e741facf6df76e2b84564fb7ad73162631
AlexxNica <kryndex@gmail.com>
2017-10-31 13:43:34 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 
 import * as RegClient from 'npm-registry-client';
 import * as semver from 'semver';
+import * as chalk from 'chalk';
 
 const packageName = process.env.CUSTOM_PACKAGE_NAME;
-
 const client = new RegClient({});
 const uri = "https://registry.npmjs.org/npm";
 const params = {
@@ -15,16 +15,19 @@ const params = {
 
 client.distTags.fetch(uri, params, function (error, data, raw, res) {
   const dataArray = (<any>Object).entries(data);
-  const versionsList: string[] = [];
+  const versionsListDummy: string[] = [];
 
 
   for (const prop in data) {
-    versionsList.push(`${prop} = ${data[prop]}`);
-  }
+    versionsListDummy.push(`${prop} = ${data[prop]}`);
+  };
+
+  const versionsList = versionsListDummy.reduce(function (accumulator, currentValue) {
+    return accumulator + '\n' + currentValue;
+  });
 
   console.log();
-  console.log(`Latest versions of the package ${packageName}:`);
-  console.log();
+  console.log((<any>chalk).blue(`Latest versions of the package ${packageName}:`));
   console.log(versionsList);
   console.log(
     // (<any>Object).values(data).sort(semver.rcompare)[0]
