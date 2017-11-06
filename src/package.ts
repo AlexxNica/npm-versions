d8d12d09fae878b4d1826b24e1dfab9db155ce74
AlexxNica <kryndex@gmail.com>
2017-11-06 13:59:55 -0200

    update-working

--- src/package.ts
+++ src/package.ts
@@ -7,12 +7,8 @@ const defaultOptions = {
 };
 
 const getPackage = (options): Promise<any> => {
-  // let packageName = (typeof options === 'string' ? options : undefined);
   options = Object.assign(defaultOptions, (typeof options === 'string' ? { packageName: options, } : options));
 
-  // if (typeof packageName !== 'undefined') {
-  //   options.packageName = packageName
-  // };
   if (options.packageName.length === 0) {
     log.error('You need to pass at least one package name to the command.');
     process.exit(1);
