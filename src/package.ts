423c413f4ed5c02ba6ca6ccd361e04f9a94acfa6
AlexxNica <kryndex@gmail.com>
2017-11-06 14:07:43 -0200

    update-working

--- src/package.ts
+++ src/package.ts
@@ -1,23 +1,30 @@
+/************
+** IMPORTS **
+************/
 import fetch from './fetch';
 import log from './log';
 
+/* Default Options (used as a base, will be merged and overwritten by the options passed to function) */
 const defaultOptions = {
   registryUrl: 'https://registry.npmjs.org/',
 };
 
-const getPackage = (options): Promise<any> => {
+/* Function that will be exported */
+const getPackage = async (options): Promise<any> => {
   options = Object.assign(defaultOptions,
     (typeof options === 'string' ? { packageName: options, } : options)
   );
 
+  // Check if any package has been specified. Throw error if not.
   if (options.packageName.length === 0) {
     log.error('You need to pass at least one package name to the command.');
     process.exit(1);
   }
 
-  options.registryUrl = options.registryUrl || defaultOptions.registryUrl;
-
-  return fetch(options);
+  return await fetch(options);
 };
 
+/************
+** EXPORTS **
+************/
 export default getPackage;
