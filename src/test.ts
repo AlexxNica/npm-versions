2657f4ee8361a97a94012a1dcefbd59b402af353
AlexxNica <kryndex@gmail.com>
2017-11-08 11:41:49 -0200

    update-working|testing

--- src/test.ts
+++ src/test.ts
@@ -1,55 +1,65 @@
-// var jsonPromise = new Promise(function (resolve, reject) {
-//   resolve(JSON.parse(`{
-//     "heading": "<h1>A story about something</h1>",
-//     "chapterUrls": [
-//       "chapter-1.json",
-//       "chapter-2.json",
-//       "chapter-3.json",
-//       "chapter-4.json",
-//       "chapter-5.json"
-//     ]
-//   }`));
-// });
+/* IMPORTS */
+import * as net from 'net';
 
-// jsonPromise.then(function (data) {
-//   console.log('It worked!\n', data);
-// }).catch(function (err) {
-//   console.log('It failed!\n', err);
-// }).then(function(){
-//   console.log('Aftermath.');
-// }).then(function(){
-//   console.log('Another aftermath!');
-// })
+/* VARIABLES */
+const socket = new net.Socket();
+let msg = '';
 
-import * as querystring from 'querystring';
-import * as https from 'https';
+/* FUNCTIONS */
+// function chr(n) {
+//   if (n < 128) {
+//     return String.fromCharCode(n);
+//   } else {
+//     return "ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ "[n - 128];
+//   }
+// }
 
-const options = {
-  hostname: 'registry.npmjs.org',
-  port: 443,
-  path: '/-/package/babel-eslint/dist-tags',
-  method: 'GET',
-  headers: {
-    'Accept': 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
-  }
-};
-
-const req = https.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
+/* LOGIC */
+// Looking Up
+socket.on('lookup', () => {
+  console.log('Looking up...');
 });
 
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
+// Connecting
+socket.connect({host: 'go.shivera-global.net', port: 7171}, () => {
+  console.log(`Connecting...`);
 });
 
-req.end();
+socket.setEncoding('utf8');
 
-export default req;
+// Connected
+socket.on('connect', () => {
+  console.log('Connected!');
+  const buff = new Buffer([0x06, 0x00, 0xFF, 0xFF, 0x69, 0x6E, 0x66, 0x6F]);
+  socket.write(buff);
+  // socket.write('\x06\x00\xFF\xFF\x69\x6E\x66\x6F');
+  // console.log(`${socket.bufferSize}`);
+  // console.log(`${socket.bytesRead}`);
+  // console.log(`${socket.bytesWritten}`);
+  // console.log(`${socket.bufferSize}`);
+});
+
+// Data Received
+socket.on('data', (data) => {
+  console.log('Received data!');
+  msg += data.toString();
+  // socket.destroy();
+});
+
+
+// Ended
+socket.on('end', () => {
+  console.log('Socket ended.');
+  console.log(msg);
+});
+
+// Error
+socket.on('error', (err) => {
+
+  console.log(`ERROR! ${JSON.stringify(err)}`);
+});
+
+// Timed Out
+socket.on('timeout', () => {
+  console.log('Timed out.');
+});
